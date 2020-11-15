d6f1facda1bea968da1bb09f59e3936694020f2f
EdersonDav <silva.edersonqueiroz@gmail.com>
2020-11-15 16:24:22 -0300

    Adicionando status code nas respostas de erro

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,14 @@ routes.get('/search/:key', (require, response) => {
   const keyAndValue = require.params.key
   const [key, value] = keyAndValue.split("-")
   const searchEmployee = employeesMethods.search(key, value );
+
+  if(searchEmployee.message == 'Invalid Key'){
+    response.status(400).json(searchEmployee)
+  }
+  if(searchEmployee.length == 0){
+    response.status(404).json({message: "Employees not fount"})
+  }
+
   response.json(searchEmployee)
 })
 
@@ -20,6 +28,10 @@ routes.get('/salary/:range', (require, response) => {
   const range = require.params.range
   const [min, max] = range.split("-")
   const bySalaryRange = employeesMethods.salaryRange(min, max);
+
+  if(bySalaryRange.message == 'Invalid Salary' ||  bySalaryRange.message == 'Minimum value cannot be greater than maximum value'){
+    response.status(400).json(bySalaryRange)
+  }
   response.json(bySalaryRange)
 })
 
@@ -32,7 +44,11 @@ routes.get('/', (require, response) => {
 routes.delete('/:cpf', (require, response) => {
   const {cpf} = require.params
   const newEmployees = employeesMethods.deleteEmployee(cpf)
-  response.json(newEmployees)
+
+  if(newEmployees.message == 'Invalid CPF' ||  newEmployees.message == 'CPF not found'){
+    response.status(400).json(newEmployees)
+  }
+  response.status(200).json(newEmployees)
 })
 
 routes.post('/', (require, response) => {
@@ -41,7 +57,12 @@ routes.post('/', (require, response) => {
   const employeeCreate = {dataCadastro, cargo, cpf, nome, ufNascimento, salario, status}
 
   const newEmployees = employeesMethods.createOrUpdate(employeeCreate)
-  response.json(newEmployees)
+
+  if(newEmployees.message != "Updated employee"||  newEmployees.message != "Employee created"){
+    response.status(400).json(newEmployees)
+  }
+  response.status(200).json(newEmployees)
+
 })
 
 module.exports = routes;
