6e523438d3b6d9aecc6a9a9b7c3e569506ad8d2b
EdersonDav <silva.edersonqueiroz@gmail.com>
2020-11-16 15:42:18 -0300

    Adiçao do cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
+const cors = require('cors')
 const swaggerOptions = require('./src/swagger.json')
-
-
 const routes = require('./src/routes');
 
 const app = express();
+
+app.use(cors())
 
 app.use("/swagger", swaggerUI.serve, swaggerUI.setup(swaggerOptions));
 
